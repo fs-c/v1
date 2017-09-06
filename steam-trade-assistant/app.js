0eaa5e0365e8ec1a0210597c8775945ec4c106e6
LW2904 <8392143a@gmail.com>
2017-09-06 13:59:36 +0200

    Update example to take object of account objects as input.

--- steam-trade-assistant/app.js
+++ steam-trade-assistant/app.js
@@ -1,22 +1,30 @@
 const PATH = process.env.DATA_PATH || '../../steamdata'
+const DATA = require(PATH)
 
 const log = require('./logger')
+const rls = require('readline-sync')
 
 const Trader = require('./Trader')
 
-let name = require('readline-sync').question('Account: ')
-let trader = new Trader(require(PATH)[name])
+for (let name in DATA) {
+  if (rls.keyInYN(`Use account ${name}?`)) build(name)
+}
 
-trader.on('ready', () => log.info(`trader ready.`))
+function build (name) {
+  let trader = new Trader(DATA[name])
 
-trader.on('clientError', (err, res) => log.error(`error while logging in: ${res || err}`))
-trader.on('managerError', err => log.error(`trade manager error: ${err.message || err}`))
+  trader.on('ready', () => log.info(`[${name}] trader ready.`))
 
-trader.on('newOffer', offer => {
-  log.info(`new offer (${offer.id}) by ${offer.user ? offer.user.name : offer.partner.toString()}, ${offer.itemsToGive.length} item(s) to give and ${offer.itemsToReceive.length} item(s) to receive.`)
-  if (require('readline-sync').keyInYN(`Accept offer ${offer.id}?`)) {
-    trader.accept(offer)
-    .then(() => log.info(`accepted offer ${offer.id}.`))
-    .catch(err => log.warn(`error accepting offer ${offer.id}: ${err.msg || err.message || err}`))
-  }
-})
+  trader.on('clientError', (err, res) => log.error(`[${name}] error while logging in: ${res || err}`))
+  trader.on('managerError', err => log.error(`[${name}] trade manager error: ${err.message || err}`))
+
+  trader.on('newOffer', offer => {
+    let partner = offer.user ? offer.user.name : offer.partner.toString()
+    log.info(`[${name}] new offer (${offer.id}) by ${partner}, ${offer.itemsToGive.length} item(s) to give and ${offer.itemsToReceive.length} item(s) to receive.`)
+    if (rls.keyInYN(`Accept offer ${offer.id} by ${partner} for ${name}?`)) {
+      trader.accept(offer)
+      .then(() => log.info(`[${name}] accepted offer ${offer.id}.`))
+      .catch(err => log.warn(`[${name}] error accepting offer ${offer.id}: ${err.msg || err.message || err}`))
+    }
+  })
+}
