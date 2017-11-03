7c1a90ae089387bccdab55004d8d2d160cf7f1f9
fsoc <8392143a@gmail.com>
2017-11-03 20:30:24 +0100

    Small update.

--- steam-gamekey-redeemer/index.js
+++ steam-gamekey-redeemer/index.js
@@ -1,34 +1,33 @@
 const accounts = require('./accounts')
 
-const file = require('fs').readFileSync('keys.txt')
-const used = require('fs').readFileSync('used.txt')
+const newKeys = require('fs').readFileSync('keys.txt')
+const usedKeys = require('fs').readFileSync('used.txt')
 
 const { StringDecoder } = require('string_decoder')
 const decoder = new StringDecoder('utf8')
 
-let f = decoder.write(Buffer.from(file))
-let u = decoder.write(Buffer.from(used))
+let f = decoder.write(Buffer.from(newKeys))
+let u = decoder.write(Buffer.from(usedKeys))
 
 f = f.split('\n')
 
-let parsed = []
-for (let line of f) {
-  if (line.indexOf('\r') !== -1) line = line.replace('\r', '')
-  if (line.indexOf('\t') !== -1) line = line.replace('\t', '')
-  parsed.push(line)
+function parse (f) {
+  let parsed = []
+  for (let line of f) {
+    if (line.indexOf('\r') !== -1) line = line.replace('\r', '')
+    if (line.indexOf('\t') !== -1) line = line.replace('\t', '')
+    parsed.push(line)
+  }
+  return parsed
 }
 
-let parsedUsed = []
-for (let line of u) {
-  if (line.indexOf('\r') !== -1) line = line.replace('\r', '')
-  if (line.indexOf('\t') !== -1) line = line.replace('\t', '')
-  parsedUsed.push(line)
-}
+let parsed = parse(f)
+let used = parse(u)
 
 console.log(`redeeming ${parsed.length} keys on ${accounts.length} accounts.`)
 
 let keys = []
-let size = parsed.length / accounts.length
+let size = parsed.length / accounts.length // parsed changes with every iteration, can't do this inline.
 accounts.forEach(() => keys.push(parsed.splice(0, size)))
 
 const Steam = require('steam-user')
@@ -43,9 +42,9 @@ function build (a, i) {
   bot.on('loggedOn', () => {
     console.log(`logged on with ${a.accountName}.`)
     for (let key of keys[i]) {
-      if (!parsedUsed.includes(key)) {
+      if (!used.includes(key)) {
         bot.redeemKey(key, (res, det) => {
-          console.log(`key: ${key}, result: ${Steam.EResult[res]}, details: ${Steam.EPurchaseResultDetail[det]}}`)
+          console.log(`key: ${key}, result: ${Steam.EResult[res]}, details: ${Steam.EPurchaseResultDetail[det]}`)
           if (res === 1) require('fs').appendFileSync('used.txt', key + '\n')
         })
       } else console.log(`key ${key} already used`)
