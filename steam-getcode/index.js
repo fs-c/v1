c546d8e951c37de30bc48a736868034d61cca2c5
fsoc <8392143a@gmail.com>
2018-01-20 12:35:14 +0100

    Expand slightly.

--- steam-getcode/index.js
+++ steam-getcode/index.js
@@ -1,1 +1,15 @@
-console.log(require('steam-totp').getAuthCode(require('/home/lw/.steam.json').default.shasec))
+const totp = require('steam-totp')
+
+const shasec = process.env.SHASEC
+
+totp.getTimeOffset((err, off, lat) => {
+  if (err) return console.error('err: ' + err)
+
+  console.log(`offset of ${off} with ${lat} delay`)
+
+  totp.getAuthCode(shasec, (err, code, off, lat) => {
+    if (err) return console.error('err: ' + err)    
+    
+    console.log(`code ${code} with ${lat} delay`)
+  })
+})
